1b97f5f58be5dc761108f2141de33b0af2269cad
brunocompasso1 <bruno.compasso1@gmail.com>
2021-05-07 16:12:03 -0300

    ...

--- routes/activity.js
+++ routes/activity.js
@@ -31,23 +31,23 @@ function logData(req) {
     secure: req.secure,
     originalUrl: req.originalUrl,
   });
-  console.log("body: " + util.inspect(req.body));
-  console.log("headers: " + req.headers);
-  console.log("trailers: " + req.trailers);
-  console.log("method: " + req.method);
-  console.log("url: " + req.url);
-  console.log("params: " + util.inspect(req.params));
-  console.log("query: " + util.inspect(req.query));
-  console.log("route: " + req.route);
-  console.log("cookies: " + req.cookies);
-  console.log("ip: " + req.ip);
-  console.log("path: " + req.path);
-  console.log("host: " + req.host);
-  console.log("fresh: " + req.fresh);
-  console.log("stale: " + req.stale);
-  console.log("protocol: " + req.protocol);
-  console.log("secure: " + req.secure);
-  console.log("originalUrl: " + req.originalUrl);
+//   console.log("body: " + util.inspect(req.body));
+//   console.log("headers: " + req.headers);
+//   console.log("trailers: " + req.trailers);
+//   console.log("method: " + req.method);
+//   console.log("url: " + req.url);
+//   console.log("params: " + util.inspect(req.params));
+//   console.log("query: " + util.inspect(req.query));
+//   console.log("route: " + req.route);
+//   console.log("cookies: " + req.cookies);
+//   console.log("ip: " + req.ip);
+//   console.log("path: " + req.path);
+//   console.log("host: " + req.host);
+//   console.log("fresh: " + req.fresh);
+//   console.log("stale: " + req.stale);
+//   console.log("protocol: " + req.protocol);
+//   console.log("secure: " + req.secure);
+//   console.log("originalUrl: " + req.originalUrl);
 }
 
 /*
@@ -120,7 +120,7 @@ exports.execute = function (req, res) {
 
       // SINCH TEST
 
-      logData(req);
+    //   logData(req);
       res.send(200, "Execute");
     } else {
       console.error("inArguments invalid.");
