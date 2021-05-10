fd065d4028f709a09749fe2cad9027c1f004df76
brunocompasso1 <bruno.compasso1@gmail.com>
2021-05-10 09:12:23 -0300

    ...

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -99,7 +99,7 @@ define([
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.Pedidos_Test.EmailAddress}}",
+            "emailAddress": "{{InteractionDefaults.Email}}",
             "test": "???"
         }];
         
