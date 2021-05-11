c74a7d07ccd94250529fe12b8ce095da36f449c7
brunocompasso1 <bruno.compasso1@gmail.com>
2021-05-11 09:45:57 -0300

    ...

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
             "emailAddress": "{{InteractionDefaults.Email}}",
-            "phoneNumber": "{{Contact.Default.Phone}}",
+            "xxxxxxx": "{{Contact.Key}}",
             "test": "???"
         }];
         
