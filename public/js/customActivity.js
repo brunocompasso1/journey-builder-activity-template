0dca1c9d8093c8113db3911463db42551a8c34db
brunocompasso1 <bruno.compasso1@gmail.com>
2021-05-11 08:20:58 -0300

    ...

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -101,7 +101,7 @@ define([
             "tokens": authTokens,
             // "Phone": "{{InteractionDefaults.Phone}}",
             // "phoneNumber": "{{Contact.Default.PhoneNumber}}",
-            "phoneNumber": "{{Contact.Default.PhoneNumber}}",
+            "phoneNumber": "{{InteractionDefaults.Phone}}",
             "test": "???"
         }];
         
